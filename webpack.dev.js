262b32e153b011181afda807adceb3e14322126e
Svein-Tore Landsverk <sveintore.landsverk@gmail.com>
2023-02-04 14:51:31 +0100

    devServer static

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,8 +7,6 @@ module.exports = merge(common, {
     // to mimic GitHub Pages serving 404.html for all paths
     // and test spa-github-pages redirect in dev
     devServer: {
-        historyApiFallback: {
-            rewrites: [{ from: /\//, to: '/404.html' }],
-        },
+        static: './'
     },
 });
